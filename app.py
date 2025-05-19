20aa6070b968fec0fe7642f2183e3c2ac1a83128
vanshgupta <vanshgupta1810@gmail.com>
2025-05-19 22:31:39 +0530

    final

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 from time import time
-from plotly.subplots import make_subplotsg
+from plotly.subplots import make_subplots
+
 
 # App Configuration
 st.set_page_config(page_title="Lawyer Predictor", layout="wide")
